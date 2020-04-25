2ca064a2025a242b22a527d03dbf95744bd8847b
Funsho Fatoye <fatoyefunsho@gmail.com>
2020-04-25 22:46:51 +0100

    added details screen

--- lib/main.dart
+++ lib/main.dart
@@ -56,12 +56,44 @@ class _MyListScreenState extends State {
           itemCount: users.length,
           itemBuilder: (context, index) {
             return ListTile(
-                leading: CircleAvatar(
-                    backgroundImage: NetworkImage(
-                        'https://res.cloudinary.com/djxdnzlej/image/upload/v1554376001/pp.jpg')),
-                title: Text(users[index].name),
-                subtitle: Text(users[index].email));
+              leading: CircleAvatar(
+                  backgroundImage: NetworkImage(
+                      'https://res.cloudinary.com/djxdnzlej/image/upload/v1554376001/pp.jpg')),
+              title: Text(users[index].name),
+              subtitle: Text(users[index].email),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => DetailScreen(),
+                    // Pass the arguments as part of the RouteSettings. The
+                    // DetailScreen reads the arguments from these settings.
+                    settings: RouteSettings(
+                      arguments: users[index],
+                    ),
+                  ),
+                );
+              },
+            );
           },
         ));
+  }
+}
+
+class DetailScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final User users = ModalRoute.of(context).settings.arguments;
+
+    // Use the Todo to create the UI.
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(users.name),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Text(users.email),
+      ),
+    );
   }
 }
